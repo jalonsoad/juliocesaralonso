34b94521f875563c5e4eb841a60975cea2810345
Julio Cesar Alonso Valerio <jcalonsov@julios-imac.home>
2016-05-26 11:00:16 +0100

    new setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,21 @@
 
-lock '3.3.5'
+lock '3.5.0'
 
 set :application, 'juliocesaralonso.com'
 set :repo_url, 'git@github.com:jalonsoad/juliocesaralonso.git'
 
 
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+
+# setup rvm.
+set :rbenv_type, :user
+#set :rbenv_ruby, '2.3.1'
+set :rbenv_path, '/usr/bin'
+
+#set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+#set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+
 
 set :use_sudo, false
 set :bundle_binstubs, nil
@@ -18,148 +28,4 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:reload'
   end
-end
-
-
-
-
-production rb
-********
-set :stage, :production
-set :branch, "master"
-
-# used in case we're deploying multiple versions of the same
-# app side by side. Also provides quick sanity checks when looking
-# at filepaths
-set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
-set :server_name, "www.juliocesaralonso.com juliocesar.com"
-
-server '146.255.96.152', user: 'deploy', roles: %w{web app db}, primary: true
-
-set :deploy_to, "/var/www/vhost/juliocesaralonso.com/apps/#{fetch(:full_app_name)}"
-
-# dont try and infer something as important as environment from
-# stage name.
-set :rails_env, :production
-
-# number of unicorn workers, this will be reflected in
-# the unicorn.rb and the monit configs
-set :unicorn_worker_count, 5
-
-# whether we're using ssl or not, used for building nginx
-# config file
-set :enable_ssl, false
-*****************
-
-
-
-set :application, 'juliocesaralonso.com'
-set :full_app_name, 'juliocesaralonso'
-set :host, "146.255.96.152"
-set :deploy_user, 'deploy'
-
-
-set :pty, true
-set :use_sudo, false
-
-
-# setup repo details
-set :scm, :git
-set :repo_url, 'git@github.com:jalonsoad/juliocesaralonso.git'
-
-# setup rvm.
-set :rbenv_type, :user
-set :rbenv_ruby, '2.3.1'
-#set :rbenv_path, '/usr/bin'
-
-#set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-
-# how many old releases do we want to keep
-set :keep_releases, 5
-
-# files we want symlinking to specific entries in shared.
-set :linked_files, %w{config/database.yml}
-
-# dirs we want symlinking to shared
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# what specs should be run before deployment is allowed to
-# continue, see lib/capistrano/tasks/run_tests.cap
-set :tests, []
-
-#Asset Management
-set :assets_roles, [:app]
-
-
-# which config files should be copied by deploy:setup_config
-# see documentation in lib/capistrano/tasks/setup_config.cap
-# for details of operations
-set(:config_files, %w(
-  nginx.conf
-  database.example.yml
-  log_rotation
-  monit
-  unicorn.rb
-  unicorn_init.sh
-))
-
-# which config files should be made executable after copying
-# by deploy:setup_config
-set(:executable_config_files, %w(
-  unicorn_init.sh
-))
-
-# files which need to be symlinked to other parts of the
-# filesystem. For example nginx virtualhosts, log rotation
-# init scripts etc.
-set(:symlinks, [
-  {
-    source: "nginx.conf",
-    link: "/etc/nginx/sites-enabled/#{fetch(:full_app_name)}"
-  },
-  {
-    source: "unicorn_init.sh",
-    link: "/etc/init.d/unicorn_#{fetch(:full_app_name)}"
-  },
-  {
-    source: "log_rotation",
-   link: "/etc/logrotate.d/#{fetch(:full_app_name)}"
-  },
-  {
-    source: "monit",
-    link: "/etc/monit/conf.d/#{fetch(:full_app_name)}.conf"
-  }
-])
-
-
-# this:
-# http://www.capistranorb.com/documentation/getting-started/flow/
-# is worth reading for a quick overview of what tasks are called
-# and when for `cap stage deploy`
-
-namespace :deploy do
-  # make sure we're deploying what we think we're deploying
-  before :deploy, "deploy:check_revision"
-  # only allow a deploy with passing tests to deployed
-  before :deploy, "deploy:run_tests"
-  # compile assets locally then rsync
- # after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
-  after :finishing, 'deploy:cleanup'
-
-  # remove the default nginx configuration as it will tend
-  # to conflict with our configs.
-  before 'deploy:setup_config', 'nginx:remove_default_vhost'
-
-  # reload nginx to it will pick up any modified vhosts from
-  # setup_config
-  after 'deploy:setup_config', 'nginx:reload'
-
-  # Restart monit so it will pick up any monit configurations
-  # we've added
-  after 'deploy:setup_config', 'monit:restart'
-
-  # As of Capistrano 3.1, the `deploy:restart` task is not called
-  # automatically.
-  after 'deploy:publishing', 'deploy:restart'
 end
